bd2d42075a4b2c5cee1f85f5f7dc735c4629dfbc
softwareNinja-888 <mlandvodlamini03@gmail.com>
2025-01-03 13:31:40 +0200

    Add repos from github api

--- portfolio/src/components/ProjectDiv.jsx
+++ portfolio/src/components/ProjectDiv.jsx
@@ -1,17 +1,27 @@
 import place from '/pro2.png'
 import github from '/github.webp'
+import external from '/external.png'
 
-export function ProjectDiv(){
+
+export function ProjectDiv({name,link,linkGit,img}){
     return (
         <>
             <div className="text-fontColor border-fontColor border w-8/12 mt-16 self-center">
                 <div className="flex justify-between text-center font-nunito py-4 px-1">
                     <div className="">
-                    Project Title
+                    {name}
                     </div>    
                     <div className="flex gap-3">
-                        <img src={github} alt="" className="w-5 h-5" />
-                        <img src={github} alt="" className="w-5 h-5" />
+
+                        {/* GITHUB */}
+                        <a href={linkGit} target="_blank" >
+                            <img src={github} alt="" className="w-5 h-5" />
+                        </a>
+
+                        {/* LIVE LINK*/}
+                        <a href={link} target="_blank" >
+                            <img src={external} alt="" className="w-5 h-5" />
+                        </a>
                     </div>
                 </div>  
                 <div className="">
--- portfolio/src/components/Projects.jsx
+++ portfolio/src/components/Projects.jsx
@@ -3,21 +3,55 @@ import dev from '/dev.png'
 import reponsive from '/responsive.png'
 
 import { useInView } from 'framer-motion';
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 
 import {ProjectDiv} from './ProjectDiv'
 
+// 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 DO NOT DELETE
+
 export function Projects(){
+    
+    const [repos, setRepos] = useState([]); // Initialize state as an array
+    const repositories = [
+        { owner: "softwareNinja-888", name: "salt_light" ,vercel:'https://salt-light-9h3y.vercel.app/'},
+        { owner: "softwareNinja-888", name: "FlixHub" ,vercel:'https://flix-hub-lac.vercel.app/'},
+        { owner: "softwareNinja-888", name: "financeDashboard" ,vercel:'https://finance-dashboard-tan-ten.vercel.app/'},
+        { owner: "softwareNinja-888", name: "cv" ,vercel:'https://cv-omega-three.vercel.app/'},
+
+    ];
+
+    useEffect(() => {
+      const fetchRepos = async () => {
+        try {
+          const promises = repositories.map(async repo => {
+            const response = await fetch(`https://api.github.com/repos/${repo.owner}/${repo.name}`);
+            if (!response.ok) {
+              throw new Error(`Failed to fetch: ${response.statusText}`);
+            }
+            const data = await response.json();
+            return { ...data, vercelLink: `${repo.vercel}` }; // Add Vercel link
+          });
+          const data = await Promise.all(promises);
+          setRepos(data);
+        } catch (error) {
+          console.error("Error fetching repositories:", error);
+        }
+      };
+  
+      fetchRepos();
+    }, []);
+
+    console.log(repos); // Logs the fetched repository data
+
     return (
         <>
 
             <div>
                 <div className="text-fontColor text-3xl text-center font-poppins mt-20 underline">My Projects</div>
                 <div className="grid grid-cols-1 justify-items-center w-screen">
-                    <ProjectDiv/>
-                    <ProjectDiv/>
-                    <ProjectDiv/>
-                    <ProjectDiv/>
+                    {repos.map(repo=>{
+                      return <ProjectDiv key={repo.id} name={repo.name} link={repo.vercelLink} linkGit={repo.html_url + '/blob/main/README.md'}/>
+                    })}
                 </div>
               
             </div>
